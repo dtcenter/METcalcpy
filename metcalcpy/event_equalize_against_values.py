3594aac87ecead781e7fd45e84a3894bdf5c6441
TatianaBurek <tatiana@ucar.edu>
2019-10-08 09:31:49 -0600

    initial version

---
+++ metcalcpy/event_equalize_against_values.py
@@ -0,0 +1,58 @@
+"""
+Program Name: event_equalize_against_values.py
+"""
+
+import pandas as pd
+
+__author__ = 'Tatiana Burek'
+__version__ = '0.1.0'
+__email__ = 'met_help@ucar.edu'
+
+
+def event_equalize_against_values(series_data, indy_var, input_unique_cases):
+    """Performs event equalisation.
+
+    event_equalize_against_values assumes that the input series_data contains data
+    indexed by fcst_valid, series values and the independent variable values.
+    It builds a new data frame which contains the same
+    data except for records that don't have corresponding fcst_valid
+    and fcst_lead values from ee_stats_equalize
+
+    Args:
+        series_data: data frame containing the records to equalize, including fcst_valid_beg, series
+                  values and independent variable values
+        indy_var: name of the independent variable
+                - not used. Keep for now for the consistency with R function
+        input_unique_cases: unique cases to equalize against
+    Returns:
+        A data frame that contains equalized records or empty frame
+    """
+
+    warning_remove = "WARNING: event equalization removed {} rows"
+
+    column_names = list(series_data)
+
+    if 'fcst_valid' in column_names:
+        # always use fcst_valid for equalization
+
+        # create a unique member to use for equalization
+        series_data['equalize'] = series_data["fcst_valid"].astype(str) \
+                                  + ' ' + series_data["fcst_lead"].astype(str)
+    else:
+        print("WARNING: eventEqualize() did not run due to lack of valid time field")
+        return pd.DataFrame()
+
+    # create an equalized set of data for the minimal list of dates based on the input cases
+    data_for_unique_cases = series_data[(series_data['equalize'].isin(input_unique_cases))]
+    n_row_cases = len(data_for_unique_cases)
+    if n_row_cases == 0:
+        print(" WARNING: discarding all members")
+        return pd.DataFrame()
+
+    n_row_ = len(series_data)
+    if n_row_cases != n_row_:
+        print(warning_remove.format(n_row_ - n_row_cases))
+
+    # remove 'equalize' column
+    data_for_unique_cases = data_for_unique_cases.drop(['equalize'], axis=1)
+    return data_for_unique_cases
---
+++ test/test_event_equalize_against_values.py
@@ -0,0 +1,52 @@
+"""Tests the operation of METcalcpy's event_equalize_against_values code."""
+
+import pandas as pd
+
+from metcalcpy.event_equalize_against_values import event_equalize_against_values
+
+
+def test_event_equalize_against_values():
+    """Tests event equalization against values."""
+
+    indy_var = "fcst_lead"
+    series_val = dict({'model': ["AFWAOCv3.5.1_d01", "NoahMPv3.5.1_d01"]})
+
+    fcst_var_val = dict({'APCP_03': ["RATIO_FSA_ASA"]})
+    input_data_file = '/Users/tatiana/ee_testing/ee_av_input.data'
+    stats_input_data_file = '/Users/tatiana/ee_testing/stats_ee_av_input.data'
+    output_data_file = '/Users/tatiana/ee_testing/ee_av_output_py.data'
+
+    # read the input data file into a data frame
+    input_data = pd.read_csv(input_data_file, header=[0], sep='\t')
+    stats_data = pd.read_csv(stats_input_data_file, header=[0], sep='\t')
+    output_data = pd.DataFrame()
+
+    for fcst_var, fcst_var_stats in fcst_var_val.items():
+        for series_var, series_var_vals in series_val.items():
+            # ungroup series value
+            series_var_vals_no_group = []
+            for val in series_var_vals:
+                split_val = val.split(',')
+                series_var_vals_no_group.extend(split_val)
+
+            ee_stats_equalize = input_data[
+                (input_data['fcst_var'] == fcst_var)
+                & (input_data[series_var].isin(series_var_vals_no_group))
+                ]
+            f_plot = stats_data[(stats_data['fcst_var'] == fcst_var)
+                                & (stats_data[series_var].isin(series_var_vals_no_group))
+                                ]
+            ee_stats_equalize_unique = (list(set(ee_stats_equalize['equalize'])))
+            f_plot = event_equalize_against_values(f_plot, indy_var, ee_stats_equalize_unique)
+
+            # append EE data to result
+            if output_data.empty:
+                output_data = f_plot
+            else:
+                output_data.append(f_plot)
+        # save to file
+        output_data.to_csv(index=False, sep='\t', path_or_buf=output_data_file)
+
+
+if __name__ == "__main__":
+    test_event_equalize_against_values()
