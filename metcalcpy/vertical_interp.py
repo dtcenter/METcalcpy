4bdbd73203150f4e79f7ca5364d72b1609fdbf35
davidfillmore <fillmore.winslow.david@gmail.com>
2020-09-01 12:29:57 -0600

    Initial commit of vertical_interp script.

---
+++ metcalcpy/vertical_interp.py
@@ -0,0 +1,52 @@
+"""
+Program Name: vertical_interp.py
+
+A python script to vertically interpolate fields
+between grids with pressure or height vertical coordinates.
+
+Version  Date
+0.1.0    2020/09/01  David Fillmore  Initial version
+"""
+
+__author__ = 'David Fillmore'
+__version__ = '0.1.0'
+__email__ = 'met_help@ucar.edu'
+
+"""
+Import standard modules
+"""
+import os
+import sys
+import argparse
+import logging
+import math
+import numpy as np
+
+if __name__ == '__main__':
+    """
+    Parse command line arguments
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--datadir', type=str,
+        default=os.getenv('DATA_DIR'),
+        help='top-level data directory (default $DATA_DIR)')
+    parser.add_argument('--input', type=str,
+        required=True,
+        help='input file name')
+    parser.add_argument('--output', type=str,
+        required=True,
+        help='output file name')
+    parser.add_argument('--logfile', type=str, 
+        default=sys.stdout,
+        help='log file (default stdout)')
+    parser.add_argument('--debug', action='store_true',
+        help='set logging level to debug')
+    args = parser.parse_args()
+
+    """
+    Setup logging
+    """
+    logging_level = logging.DEBUG if args.debug else logging.INFO
+    logging.basicConfig(stream=args.logfile, level=logging_level)
+    logging.info(args.datadir)
+    logging.info(args.filename)
