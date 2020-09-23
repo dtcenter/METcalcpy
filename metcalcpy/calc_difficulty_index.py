a92c7f694f769e2ff44690fdb9d603641b24e793
Lindsay Blank <lblank@eyewall.rap.ucar.edu>
2020-09-23 22:32:45 +0000

    Difficulty index staging

---
+++ metcalcpy/calc_difficulty_index.py
@@ -0,0 +1,239 @@
+# -*- coding: utf-8 -*-
+"""
+
+Forecast decision difficulty indices.
+
+Implement a set of decision difficulty indices for forecasts of postive
+definite quantities such as wind speed and wave height.
+
+Created on Thu Feb 20 16:23:06 2020
+Last modified on Mon Mar 30 11:25:30 2020
+@author: campbell
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from .piecewise_linear.py import PiecewiseLinear as plin
+import mycolormaps as mcmap
+
+# Enforce positive definiteness of quantities such as standard deviations
+EPS = np.finfo(np.float32).eps
+# Only allow 2D fields for now
+FIELD_DIM = 2
+
+def _input_check_v4567(sigmaij, muij, threshold, fieldijn,
+                       sigma_over_mu_ref, under_factor):
+    """
+    Check for valid input to index_v456.
+
+    Parameters
+    ----------
+    sigmaij : 2D numpy array
+        Positive definite array of standard deviations of a 2D field.
+    muij : Float scalar or 2D numpy array
+        The mean values corresponding to sigmaij.
+    threshold : Float (or int) scalar
+        A significant value to be compared with values of the forecast field
+        for each ensemble member.
+    sigma_over_mu_ref : Scalar
+    fieldijn : 3D numpy array
+        Values of the forecast field. Third dimension is ensemble member.
+    under_factor : Float scalar
+        Must be between 0 and 1.
+
+    Returns
+    -------
+    None.
+
+    """
+    assert isinstance(threshold, (int, float,
+                                  np.int32, np.float32))
+    assert np.ndim(sigmaij) == FIELD_DIM
+    assert np.all(sigmaij) >= EPS
+    fieldshape = np.shape(fieldijn)
+    # muij is a scalar or 2D array
+    if isinstance(muij, np.ndarray):
+        # If muij is an array, it must have the same shape as sigmaij
+        assert np.shape(muij) == np.shape(sigmaij)
+    assert sigma_over_mu_ref >= EPS
+    assert np.shape(sigmaij) == tuple(np.squeeze(fieldshape[0:-1]))
+    assert isinstance(under_factor, (int, float,
+                                     np.int32, np.float32))
+    assert 0.0 <= under_factor <= 1.0
+
+
+def _index_v4567(sigmaij, muij, threshold, fieldijn,
+                 Aplin, sigma_over_mu_ref=EPS, under_factor=0.5):
+    """
+    Versions 4, 5, 6, 6.1, and 7 of forecast difficulty index.
+
+    Main difference between versions is the envelope function Aplin.
+    Secondary difference between 4 and the others is the under_factor
+    The threshold terms all penalize equal (or slightly unequal) spread.
+
+    Parameters
+    ----------
+    sigmaij : 2D numpy array
+        Positive definite array of standard deviations of a 2D field.
+    muij : Float scalar or 2D numpy array
+        The mean values corresponding to sigmaij.
+    threshold : Float (or int) scalar
+        A significant value to be compared with values of the forecast field
+        for each ensemble member.
+    fieldijn : 3D numpy array
+        Values of the forecast field. Third dimension is ensemble member.
+    Aplin: PiecewiseLinear object (envelope function)
+        Essentially a piecewise linear localization function based on muij
+    sigma_over_mu_ref : Scalar, optional
+        Highest value of sigmaij/muij for past 5 days (nominally).
+    under_factor: Scalar, optional
+        Defaults to 0.5 except should be 0.4 for v4 difficulty index
+
+    Returns
+    -------
+    dij : 2D numpy array
+        Normalized v4567 difficulty index ([0,1.5]). Larger (> 0.5)
+        means more difficult.
+
+    """
+    # Check for valid input
+    _input_check_v4567(sigmaij, muij, threshold, fieldijn,
+                       sigma_over_mu_ref, under_factor)
+
+    # Variance term in range 0 to 1
+    sigma_over_mu = sigmaij / muij
+    sigma_over_mu_max = np.nanmax(sigma_over_mu)
+    # Force reference value to be greater than current max of sigmaij / muij
+    sigma_over_mu_ref = np.nanmax([sigma_over_mu_ref,
+                                   sigma_over_mu_max])
+    variance_term = sigma_over_mu / sigma_over_mu_ref
+
+    # Depends on under_factor, which is 0.5 except for v4
+    under_threshold_count =\
+        np.ma.masked_greater_equal(fieldijn, threshold).count(axis=-1)
+    nmembers = np.shape(fieldijn)[-1]
+    under_prob = under_threshold_count / nmembers
+
+    # Threshold term in range 0 to 1
+    threshold_term = 1.0 - np.abs(1.0 - under_factor - under_prob)
+
+    # Linear taper factor
+    taper_term = Aplin.values(muij)
+
+    # Difficulty index is the average of the two terms
+    # multiplied by a linear taper factor
+    dij = 0.5 * taper_term * (variance_term + threshold_term)
+
+    return dij
+
+
+def forecast_difficulty(sigmaij, muij, threshold, fieldijn,
+                        Aplin=None, sigma_over_mu_ref=EPS):
+    """
+    Calls private function _index_v4567,
+    version 7 of forecast difficulty index.
+
+    Parameters
+    ----------
+    sigmaij : 2D numpy array
+        Positive definite array of standard deviations of a 2D field.
+    muij : Float scalar or 2D numpy array
+        The mean values corresponding to sigmaij.
+    threshold : Float (or int) scalar
+        A significant value to be compared with values of the forecast field
+        for each ensemble member.
+    fieldijn : 3D numpy array
+        Values of the forecast field. Third dimension is ensemble member.
+    Aplin : PiecewiseLinear object (envelope function), optional
+        Essentially a piecewise linear localization function based on muij
+    sigma_over_mu_ref : Scalar, optional
+        Highest value of sigmaij/muij for past 5 days (nominally).
+
+    Returns
+    -------
+    dij : 2D numpy array
+        Normalized difficulty index ([0,1.5]).
+        Larger (> 0.5) means more difficult.
+
+    """
+    if Aplin is None:
+        # Envelope for version 6.1, the default
+        xunits = 'feet'
+        A6_1_name = "A6_1"
+        A6_1_left = 0.0
+        A6_1_right = 0.0
+        A6_1_xlist = [3.0, 9.0, 12.0, 21.0]
+        A6_1_ylist = [0.0, 1.5, 1.5, 0.0]
+        Aplin =\
+            plin.PiecewiseLinear(A6_1_xlist, A6_1_ylist, xunits=xunits,
+                                 right=A6_1_right, left=A6_1_left,
+                                 name=A6_1_name)
+
+    dij = _index_v4567(sigmaij, muij, threshold, fieldijn,
+                       Aplin, sigma_over_mu_ref)
+
+    return dij
+
+# Taking out references to plotting in main.
+# Then test dependencies 
+# Then print out arrays
+# Then test
+# Then get rid of main
+
+def main():
+    
+    
+    """
+    Test harness for difficulty indices.
+
+    Returns
+    -------
+    None.
+
+    """
+    nlon = 18
+    nlat = 9
+    nmembers = 10
+    np.random.seed(12345)
+    # Wave heights and wind speeds generally follow the Rayleigh distribution,
+    # which is a Weibull distribution with a scale factor of 2,
+    # and any shape parameter.
+    # If the shape parameter is 1, then it is the same as a chi-square
+    # distribution with 2 dofs.
+    # Expected value E[x] = sigma * sqrt(pi/2)
+    # mean_windspeed = 6.64  # meters/second
+    xunits = 'feet'
+    mean_height = 11.0  # mean wave height in feet
+    # var_height = mean_height * mean_height * ((4.0 - np.pi) / 2.0)
+    mode_height = np.sqrt(2.0 / np.pi) * mean_height
+    fieldijn = np.random.rayleigh(scale=mode_height,
+                                  size=(nlat, nlon, nmembers))
+    muij = np.mean(fieldijn, axis=-1)
+    pertijn = fieldijn - np.dstack([muij] * nmembers)
+    sigmaij = np.sqrt(np.mean(pertijn * pertijn, axis=-1))
+    threshold = 9.0
+    regularizer = 0.01
+    smax = 9.0
+    sigma_max = smax + np.zeros_like(sigmaij)
+    thresh_eps = 2.0
+    kwargs = {'thresh_eps': thresh_eps, 'threshold_type': 'proximity'}
+
+    # Envelope for version 7
+    A7_name = "A7"
+    A7_left = 0.0
+    A7_right = 0.0
+    A7_xlist = [3.0, 9.0, 12.0, 21.0]
+    A7_ylist = [0.0, 1.0, 1.0, 0.0]
+    A7 = plin.PiecewiseLinear(A7_xlist, A7_ylist, xunits=xunits,
+                              right=A7_right, left=A7_left, name=A7_name)
+    dijv7 = _index_v4567(sigmaij, muij, threshold, fieldijn,
+                         A7, sigma_over_mu_ref=EPS)
+    
+    # Test public forecast_difficulty, which should be the same as version 7
+    dijfd = forecast_difficulty(sigmaij, muij, threshold, fieldijn, Aplin=A7,
+                                sigma_over_mu_ref=EPS)
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ metcalcpy/piecewise_linear.py
@@ -0,0 +1,201 @@
+# -*- coding: utf-8 -*-
+"""
+Piecewise linear function class.
+
+Created on Thu Mar  5 08:26:59 2020
+Last modified Mon Mar 30 11:21:30 2020
+@author: campbell
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class IncompatibleLengths(Exception):
+    """Custom exception for PiecewiseLinear input checking."""
+
+
+class UnsortedArray(Exception):
+    """Custom exception for PiecewiseLinear input checking."""
+
+
+class PiecewiseLinear():
+    """
+    Defines a piecewise linear function with a given domain and range.
+
+    Xdomain is a numpy array of knot locations. Yrange is a numpy array.
+    """
+
+    def __init__(self, x_domain, y_range, xunits='feet',
+                 left=np.nan, right=np.nan, name=""):
+        len_x = len(x_domain)
+        if len_x < 2:
+            raise IncompatibleLengths('Length of xdomain must be at least 2.')
+        if np.any(np.diff(x_domain)) < 0:
+            print("X_domain (in {}) is {}".format(xunits, x_domain))
+            raise UnsortedArray('Xdomain must be sorted in ascending order.')
+        len_y = len(y_range)
+        if len_x != len_y:
+            raise IncompatibleLengths('X_domain and Y_range must have same ' +
+                                      'length.\n Use left and right to set ' +
+                                      'value for points outside the x_domain\n')
+        self.x_domain = np.array(x_domain)
+        self.y_range = np.array(y_range)
+        self.xunits = xunits
+        self.left = left
+        self.right = right
+        self.name = name
+
+    def get_ymax(self):
+        """Find maximum of envelope function"""
+        return np.max(self.y_range)
+
+    def values(self, xinput):
+        """
+        Evaluate piecewise linear function for the set of points in xinput.
+
+        xinput is a set of points inside xdomain.
+        """
+        if not isinstance(xinput, np.ndarray):
+            xinput = np.array(xinput)
+        xin_shape = np.shape(xinput)
+        # Treat xinput as one-dimensional for use by np.interp
+        xview = xinput.ravel()
+        yflat = np.interp(xview, self.x_domain, self.y_range,
+                          left=self.left, right=self.right)
+        # Restore output to same shape as input
+        youtput = np.reshape(yflat, xin_shape)
+
+        return youtput
+
+    def plot(self, x=np.nan, y=np.nan):
+        """Plot the envelope function and knots."""
+        fig, ax = plt.subplots(figsize=(15, 10))
+        # Set the tick labels font
+        for label in ax.get_xticklabels() + ax.get_yticklabels():
+            label.set_fontname('Arial')
+            label.set_fontsize(18)
+            label.set_fontweight('normal')
+        # Set the font dictionaries (for plot title and axis titles)
+        title_font = {'fontname': 'Arial', 'size': '24', 'color': 'black',
+                      'weight': 'bold', 'verticalalignment': 'bottom'}
+        axis_font = {'fontname': 'Arial', 'size': '18', 'weight': 'bold'}
+        plt.xlabel('X Domain ({})'.format(self.xunits), **axis_font)
+        plt.ylabel('Y Range', **axis_font)
+        plt.title('Piecewise Linear Test ' + self.name, **title_font)
+        plt.plot(self.x_domain, self.y_range, 'b-', linewidth=2)
+        plt.plot(self.x_domain, self.y_range, 'bo', markersize=15)
+        plt.plot(x, y, 'r*', markersize=20)
+        plt.grid(True)
+        plt.show()
+
+
+def plot_envelope_only(xvals, yvals, ttype, xunits='ft'):
+    """Plot the envelope function and knots."""
+    fig, ax = plt.subplots(figsize=(15, 10))
+    # Set the tick labels font
+    for label in ax.get_xticklabels() + ax.get_yticklabels():
+        label.set_fontname('Arial')
+        label.set_fontsize(18)
+        label.set_fontweight('normal')
+    # Set the font dictionaries (for plot title and axis titles)
+    title_font = {'fontname': 'Arial', 'size': '24', 'color': 'black',
+                  'weight': 'bold', 'verticalalignment': 'bottom'}
+    axis_font = {'fontname': 'Arial', 'size': '18', 'weight': 'normal'}
+    plt.xlabel('Mean {} ({})'.format(ttype, xunits), **axis_font)
+    plt.ylabel('Weighting on difficulty index', **axis_font)
+    plt.title('Envelope for {}'.format(ttype), **title_font)
+    plt.plot(xvals, yvals, 'r-', linewidth=4)
+    #plt.plot(self.x_domain, self.y_range, 'bo', markersize=15)
+    plt.xlim((xvals[0], xvals[-1]))
+    plt.xticks(xvals)
+    plt.ylim((np.min(yvals)-0.01, np.max(yvals)+0.11))
+    idx = np.where(yvals == np.max(yvals))[0]
+    xdata = np.tile(xvals[idx], (2, 1))
+    yzeros = np.zeros_like(yvals[idx])
+    ydata = np.array([yzeros, yvals[idx]])
+    plt.plot(xdata, ydata, 'b--')
+    plt.show()
+
+    return fig, ax
+
+
+def main():
+    """Test envelope functions."""
+    plt.close('all')
+    xunits = 'feet'
+    xmin = 0.0
+    xmax = 25.0
+    xinput = (xmax - xmin) * np.random.rand(5, 3, 2) - xmin
+    print("Unsorted inputs (in {}) are:\n {}".format(xunits, xinput))
+
+    # Envelope for versions 4 and 5
+    A45_left = 1.0
+    A45_right = 0.0
+    A45_xlist = [12.0, 21.0]
+    A45_ylist = [1.0, 0.0]
+    A45 = PiecewiseLinear(A45_xlist, A45_ylist, xunits=xunits,
+                          right=A45_right, left=A45_left, name="A4 & A5")
+    A45_youtput = A45.values(xinput)
+    print("Corresponding values are:\n {}".format(A45.values(xinput)))
+    # Pass 1D views of x and y for plotting
+    A45.plot(xinput.ravel(), A45_youtput.ravel())
+
+    # Envelope for version 6
+    A6_left = 1.0
+    A6_right = 0.0
+    A6_xlist = [6.0, 9.0, 12.0, 21.0]
+    A6_ylist = [1.0, 1.5, 1.5, 0.0]
+    A6 = PiecewiseLinear(A6_xlist, A6_ylist, xunits=xunits,
+                         right=A6_right, left=A6_left, name="A6")
+    A6_youtput = A6.values(xinput)
+    print("Corresponding values are:\n {}".format(A6.values(xinput)))
+    # Pass 1D views of x and y for plotting
+    A6.plot(xinput.ravel(), A6_youtput.ravel())
+
+    # Envelope for version 6.1
+    A6_1_left = 0.0
+    A6_1_right = 0.0
+    A6_1_xlist = [3.0, 9.0, 12.0, 21.0]
+    A6_1_ylist = [0.0, 1.5, 1.5, 0.0]
+    A6_1 = PiecewiseLinear(A6_1_xlist, A6_1_ylist, xunits=xunits,
+                           right=A6_1_right, left=A6_1_left, name="A6.1")
+    A6_1_youtput = A6_1.values(xinput)
+    print("Corresponding values are:\n {}".format(A6_1.values(xinput)))
+    # Pass 1D views of x and y for plotting
+    A6_1.plot(xinput.ravel(), A6_1_youtput.ravel())
+
+    # Envelope for version 7
+    A7_name = "A7"
+    A7_left = 0.0
+    A7_right = 0.0
+    A7_xlist = [3.0, 9.0, 12.0, 21.0]
+    A7_ylist = [0.0, 1.0, 1.0, 0.0]
+    A7 = PiecewiseLinear(A7_xlist, A7_ylist, xunits=xunits,
+                         right=A7_right, left=A7_left, name=A7_name)
+    A7_youtput = A7.values(xinput)
+    print("Corresponding values are:\n {}".format(A7.values(xinput)))
+    # Pass 1D views of x and y for plotting
+    A7.plot(xinput.ravel(), A7_youtput.ravel())
+
+    # Produce envelope figure for documentation
+    xvals = np.array([0.0] + A6_1_xlist + [24.0])
+    yvals = np.array([0.0] + A6_1_ylist + [0.0])
+    xunits = 'ft'
+    ttype = 'significant wave height'
+    fig1, ax1 = plot_envelope_only(xvals, yvals, ttype, xunits)
+    fig1.savefig('./Figure_1.png')
+
+    # Produce envelope figure for documentation
+    xvals = np.array([0.0, 5.0, 28.0, 34.0, 50.0, 55.0])
+    yvals = np.array([0.0, 0.0,  1.5,  1.5,  0.0,  0.0])
+    xunits = 'kts'
+    ttype = 'wind speed'
+    fig2, ax2 = plot_envelope_only(xvals, yvals, ttype, xunits)
+    fig2.savefig('./Figure_2.png')
+
+    return fig1, ax1, fig2, ax2
+
+
+if __name__ == "__main__":
+    FIG1, AX1, FIG2, AX2 = main()
