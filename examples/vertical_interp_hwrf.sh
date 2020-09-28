1454a5ce26ff8f06eb68728e277f72762bbbbb4f
davidfillmore <fillmore.winslow.david@gmail.com>
2020-09-28 11:24:31 -0600

    Fixed test driver script for HWRF.

--- examples/vertical_interp_hwrf.sh
+++ examples/vertical_interp_hwrf.sh
@@ -1,6 +1,6 @@
 python ../metcalcpy/vertical_interp.py \
-    --datadir $DATA_DIR/HWRF
+    --datadir $DATA_DIR/HWRF \
     --input dorian05l.2019090118.hwrfprs.core.0p015.f000.grb2 \
-    --config vertical_interp_grib.yaml \
+    --config vertical_interp_hwrf.yaml \
     --output dorian05l.2019090118.hwrfprs.core.interp.0p015.f000.grb2 \
     --debug
