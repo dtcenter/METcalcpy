e9f8896f4168d0e2f58818fe17e3caadbac99d46
davidfillmore <fillmore.winslow.david@gmail.com>
2020-10-07 15:21:22 -0600

    Renamed MERRA2 test directory.

--- examples/height_from_pressure_merra2.sh
+++ examples/height_from_pressure_merra2.sh
@@ -1,5 +1,5 @@
 python ../metcalcpy/vertical_interp.py \
-    --datadir $DATA_DIR/Events/2019-03-13-GreatPlainsCyclone \
+    --datadir $DATA_DIR/MERRA2/2019-03-13-GreatPlainsCyclone \
     --input MERRA2_400.inst3_3d_asm_Np.20190311.nc4 \
     --config vertical_interp_merra2.yaml \
     --output MERRA2_400.inst3_3d_asm_interp.20190311.nc4 \
