5a05f53e6661b6538f5c45335ee967219a3d5f2e
davidfillmore <fillmore.winslow.david@gmail.com>
2020-10-07 15:28:09 -0600

    Reogranizing data directory examples.

--- examples/vertical_interp_hwrf.sh
+++ examples/vertical_interp_hwrf.sh
@@ -1,5 +1,5 @@
 python ../metcalcpy/vertical_interp.py \
-    --datadir $DATA_DIR/HWRF \
+    --datadir $DATA_DIR/HWRF/2019-09-01-HurricaneDorian \
     --input dorian05l.2019090118.hwrfprs.core.0p015.f003.grb2 \
     --config vertical_interp_hwrf.yaml \
     --output dorian05l.2019090118.hwrfprs.core.interp.0p015.f003.nc4 \
