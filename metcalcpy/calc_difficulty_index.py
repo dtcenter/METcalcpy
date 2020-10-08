f268c0f26dea1687bab69acbc94ff0d65280bffd
Lindsay Blank <lblank@eyewall.rap.ucar.edu>
2020-10-08 20:19:42 +0000

    Public version of difficulty index and test

--- metcalcpy/calc_difficulty_index.py
+++ metcalcpy/calc_difficulty_index.py
@@ -13,7 +13,7 @@ Last modified on Mon Mar 30 11:25:30 2020
 
 import numpy as np
 import matplotlib.pyplot as plt
-from piecewise_linear import PiecewiseLinear as plin
+from metcalcpy.piecewise_linear import PiecewiseLinear as plin
 
 __author__ = 'Bill Campbell (NRL) and Lindsay Blank (NCAR)'
 __version__ = '0.1.0'
@@ -48,7 +48,7 @@ def _input_check(sigmaij, muij, threshold, fieldijn, sigma_over_mu_ref, under_fa
     None.
 
     """
-
+    print("We made it here")
     assert isinstance(threshold, (int, float, np.int32, np.float32))
     assert np.ndim(sigmaij) == FIELD_DIM
     assert np.all(sigmaij) >= EPS
@@ -64,7 +64,7 @@ def _input_check(sigmaij, muij, threshold, fieldijn, sigma_over_mu_ref, under_fa
     assert 0.0 <= under_factor <= 1.0
 
 
-def _difficuly_index(sigmaij, muij, threshold, fieldijn, Aplin, sigma_over_mu_ref=EPS, under_factor=0.5):
+def _difficulty_index(sigmaij, muij, threshold, fieldijn, Aplin, sigma_over_mu_ref=EPS, under_factor=0.5):
     """
     Calculates public version (v7) of forecast difficulty index.
     The threshold terms all penalize equal (or slightly unequal) spread.
@@ -121,14 +121,14 @@ def _difficuly_index(sigmaij, muij, threshold, fieldijn, Aplin, sigma_over_mu_re
     # Difficulty index is the average of the two terms
     # multiplied by a linear taper factor
     dij = 0.5 * taper_term * (variance_term + threshold_term)
-
+    
     return dij
 
 
 def forecast_difficulty(sigmaij, muij, threshold, fieldijn,
                         Aplin=None, sigma_over_mu_ref=EPS):
     """
-    Calls private function _index, 
+    Calls private function _difficulty_index, 
     to calculate the public version (v7) of forecast difficulty index.
 
     Parameters
@@ -165,67 +165,10 @@ def forecast_difficulty(sigmaij, muij, threshold, fieldijn,
         Aplin = plin(A7_xlist, A7_ylist, xunits=xunits,
                 right=A7_right, left=A7_left, name=A7_name)
 
-    dij = _difficuly_index(sigmaij, muij, threshold, fieldijn,
+    dij = _difficulty_index(sigmaij, muij, threshold, fieldijn,
                        Aplin, sigma_over_mu_ref)
 
     return dij
 
-# Get rid of main
-def main():
-    
-    
-    """
-    Test harness for difficulty indices.
-
-    Returns
-    -------
-    None.
-
-    """
-    nlon = 18
-    nlat = 9
-    nmembers = 10
-    np.random.seed(12345)
-    # Wave heights and wind speeds generally follow the Rayleigh distribution,
-    # which is a Weibull distribution with a scale factor of 2,
-    # and any shape parameter.
-    # If the shape parameter is 1, then it is the same as a chi-square
-    # distribution with 2 dofs.
-    # Expected value E[x] = sigma * sqrt(pi/2)
-    # mean_windspeed = 6.64  # meters/second
-    xunits = 'feet'
-    mean_height = 11.0  # mean wave height in feet
-    # var_height = mean_height * mean_height * ((4.0 - np.pi) / 2.0)
-    mode_height = np.sqrt(2.0 / np.pi) * mean_height
-    fieldijn = np.random.rayleigh(scale=mode_height,
-                                  size=(nlat, nlon, nmembers))
-    muij = np.mean(fieldijn, axis=-1)
-    pertijn = fieldijn - np.dstack([muij] * nmembers)
-    sigmaij = np.sqrt(np.mean(pertijn * pertijn, axis=-1))
-    threshold = 9.0
-    regularizer = 0.01
-    smax = 9.0
-    sigma_max = smax + np.zeros_like(sigmaij)
-    thresh_eps = 2.0
-    kwargs = {'thresh_eps': thresh_eps, 'threshold_type': 'proximity'}
-
-    # Envelope for version 7
-    A7_name = "A7"
-    A7_left = 0.0
-    A7_right = 0.0
-    A7_xlist = [3.0, 9.0, 12.0, 21.0]
-    A7_ylist = [0.0, 1.0, 1.0, 0.0]
-    A7 = plin(A7_xlist, A7_ylist, xunits=xunits,
-                              right=A7_right, left=A7_left, name=A7_name)
-    dijv7 = _difficuly_index(sigmaij, muij, threshold, fieldijn,
-                         A7, sigma_over_mu_ref=EPS)
-    
-    # Test public forecast_difficulty, which should be the same as version 7
-    dijfd = forecast_difficulty(sigmaij, muij, threshold, fieldijn, Aplin=A7,
-                                sigma_over_mu_ref=EPS)
-
-    print(dijfd)
-
-
 if __name__ == "__main__":
-    main()
+    pass
