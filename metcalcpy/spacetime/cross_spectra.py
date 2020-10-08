a489196e98d5e4dff184b462ae659aa4ff124e95
mgehne <mgehne@gmail.com>
2020-10-08 14:31:09 -0600

    remove sys import statement

--- metcalcpy/spacetime/cross_spectra.py
+++ metcalcpy/spacetime/cross_spectra.py
@@ -1,8 +1,6 @@
 import xarray as xr
 import numpy as np
-import sys
 
-sys.path.append('../../')
 """
 local scripts, if loading from a different directory include that with a '.' between
 directory name and script name
