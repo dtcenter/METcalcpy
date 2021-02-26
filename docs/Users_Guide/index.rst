f84dc6de24f13917d8f4849bc5dd47cb5918ce55
Julie.Prestopnik <jpresto@ucar.edu>
2021-02-25 17:24:00 -0700

    Made updates for documentation infrastructure

---
+++ docs/Users_Guide/index.rst
@@ -0,0 +1,92 @@
+============
+User's Guide
+============
+
+**Foreword: A note to METcalcpy users**
+
+This User's guide is provided as an aid to users of METcalcpy.  METcalcpy is a
+Python version of the statistics calculation functionality of METviewer,
+METexpress, plotting packages in METplotpy and is a stand-alone package for any
+other application. It is also a component of the unified METplus verification
+framework. More details about METplus can be found on the
+`METplus website <http://dtcenter.org/community-code/metplus>`_.
+
+It is important to note here that METcalcpy is an evolving software package.
+This documentation describes the |release| release dated |release_date|.
+Intermediate releases may include bug fixes. METcalcpy is also able to accept
+new modules contributed by the community. If you have code you would like to
+contribute, we will gladly consider your contribution. Please send an email to:
+`met_help@ucar.edu <mailto:>`__. We will then determine if we will be able to
+include the contribution in a future version.
+
+This User's Guide was prepared by the developers of METcalcpy, including
+Minna Win-Gildenmeister, Tatiana Burek, David Fillmore, John Halley Gotway,
+Dan Adriaansen, and Hank Fisher.
+
+**Model Evaluation Tools Calc Py (METcalcpy)  TERMS OF USE - IMPORTANT!**
+
+Copyright |copyright|
+Licensed under the Apache License, Version 2.0 (the "License");
+You may not use this file except in compliance with the License.
+
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+
+**Citations**
+
+The citation for this User's Guide should be:
+
+|author_list|, |release_year|: The METcalcpy Version |version| User's Guide.
+Developmental Testbed Center.
+Available at: `METcalcpy releases <https://github.com/dtcenter/METcalcpy/releases>`_
+
+**Acknowledgments**
+
+?? NOTE: THESE WERE TAKEN DIRECTLY FROM MET AND MAY NEED TO BE MODIFIED. ??
+
+We thank the National Science Foundation (NSF) along with three organizations
+within the National Oceanic and Atmospheric Administration (NOAA): 1) Office
+of Atmospheric Research (OAR); 2) Next Generation Global Prediction System
+project (NGGPS); and 3) United State Weather Research Program (USWRP), the
+United States Air Force (USAF), and the United States Department of Energy
+(DOE) for their support of this work. Funding for the development of MET-TC is
+from the NOAA's Hurricane Forecast Improvement Project (HFIP) through the
+Developmental Testbed Center (DTC). Funding for the expansion of capability to
+address many methods pertinent to global and climate simulations was provided
+by NOAA's Next Generation Global Prediction System (NGGPS) and NSF Earth
+System Model 2 (EaSM2) projects. We would like to thank James Franklin at the
+National Hurricane Center (NHC) for his insight into the original development
+of the existing NHC verification software. Thanks also go to the staff at the
+Developmental Testbed Center for their help, advice, and many types of support.
+We released METv1.0 in January 2008 and would not have made a decade of
+cutting-edge verification support without those who participated in the
+original MET planning workshops and the now dis-banded verification advisory
+group (Mike Baldwin, Matthew Sittel, Elizabeth Ebert, Geoff DiMego, Chris
+Davis, and Jason Knievel).
+
+The National Center for Atmospheric Research (NCAR) is sponsored by NSF. The
+DTC is sponsored by the National Oceanic and Atmospheric Administration (NOAA),
+the United States Air Force, and the National Science Foundation (NSF). NCAR
+is sponsored by the National Science Foundation (NSF).
+
+
+.. toctree::
+   :titlesonly:
+   :numbered: 4
+
+   installation
+   release-notes
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`search`
+    
---
+++ docs/conf.py
@@ -0,0 +1,107 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# http://www.sphinx-doc.org/en/master/config
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+print(sys.path)
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'METcalcpy'
+copyright = '2019, NCAR'
+author = 'UCAR/NCAR, NOAA, CSU/CIRA, and CU/CIRES'
+author_list = 'Win-Gildenmeister, M., Burek, T., Fillmore, D., Halley Gotway, J., Adriaansen, D., Fisher, H.'
+version = 'develop'
+verinfo = version
+release = f'{version}'
+release_year = '2021'
+release_date = f'{release_year}0331'
+copyright = f'{release_year}, {author}'
+
+# if set, adds "Last updated on " followed by
+# the date in the specified format
+html_last_updated_fmt = '%c'
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.intersphinx',
+              'sphinx_gallery.gen_gallery']
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# Suppress certain warning messages
+suppress_warnings = ['ref.citation']
+
+# -- Sphinx control -----------------------------------------------------------
+#sphinx_gallery_conf = {
+#      'examples_dirs': [os.path.join('..', 'examples')],
+#      'gallery_dirs': ['examples']
+#}
+    
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = ["_themes", ]
+html_js_files = ['pop_ver.js']
+html_theme_options = {'canonical_url': 'https://dtcenter.github.io/METcalcpy/latest/'}
+html_theme_options['versions'] = {'latest': '../latest', 'develop': '../develop'}
+html_css_files = ['theme_override.css']
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
+# The name of an image file (relative to this directory) to place at the top
+# of the sidebar.
+html_logo = os.path.join('_static','met_calcpy_logo_2019_09.png')
+
+# Control html_sidebars
+# Include global TOC instead of local TOC by default
+#html_sidebars = { '**': ['globaltoc.html','relations.html','sourcelink.html','searchbox.html']}
+
+# -- Intersphinx control -----------------------------------------------------
+
+numfig = True
+
+numfig_format = {
+        'figure': 'Figure %s',
+        }
+    
+# -- Export variables --------------------------------------------------------
+
+rst_epilog = """                                                                                                                                    
+.. |copyright|    replace:: {copyrightstr}                                                                                                          
+.. |author_list|  replace:: {author_liststr}                                                                                                        
+.. |release_date| replace:: {release_datestr}                                                                                                       
+.. |release_year| replace:: {release_yearstr}                                                                                                       
+""".format(copyrightstr    = copyright,
+           author_liststr  = author_list,
+           release_datestr = release_date,
+           release_yearstr = release_year)
+
