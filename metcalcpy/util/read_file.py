ff36b79fcdd30861d28c751fb87215af3ae94c4b
Hank Fisher <fisherh@ucar.edu>
2021-03-11 23:52:36 -0700

    Initial checkin for reading met stat output files

---
+++ metcalcpy/util/read_file.py
@@ -0,0 +1,67 @@
+"""
+Program Name: read_file.py
+Copyright 2021 UCAR/NCAR/RAL
+"""
+
+__author__ = 'Hank Fisher'
+__version__ = '1.0.0'
+__email__ = 'met_help@ucar.edu'
+
+
+import numpy as np
+import pandas as pd
+import metdbload as dbload
+
+class ReadMETOutput:
+
+    def __init__(self):
+        """ Creates a output reader with a list of files to read
+        """
+
+    def readConfigFile(self,configFile):
+        """ Returns a file or list of files
+        """
+
+        # Retrieve the contents of the custom config file to over-ride
+        # or augment settings defined by the default config file.
+        with open(configFile, 'r') as stream:
+            try:
+               files = yaml.load(stream, Loader=yaml.FullLoader)
+            except yaml.YAMLError as exc:
+                print(exc)
+
+        print(files)
+
+    def readData(self):
+        """ Returns a pandas DataFrame containing MET output contents
+        """
+
+        # instantiate a read data files object
+        file_data = dbload.ReadDataFiles()
+
+        # read in the data files, with options specified by XML flags
+        #set load_flags and line_types as empty so that everything is read
+        load_flags = []
+        line_types = []
+        file_data.read_data(load_flags,
+                            current_files,
+                            line_types)
+
+
+        df = file_data.stat_data
+        return df
+
+def main():
+    """
+    Reads in a default config file that loads sample MET output data and puts it into a pandas
+    DataFrame
+    """
+    configFile = "./readfiles.yaml"
+    file_reader = ReadMETOutput()
+    file_reader.readConfigFile(configFile)
+    df = file_reader.readData(self)
+
+
+if __name__ == "__main__":
+    main()
+
