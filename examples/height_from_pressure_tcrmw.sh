5abe2a515a32c2e635f360a8e87f805a9de3a0ff
davidfillmore <fillmore.winslow.david@gmail.com>
2021-08-31 10:11:26 -0600

    Adding example script and config for vertical interpolation of TCRMW output.

---
+++ examples/height_from_pressure_tcrmw.sh
@@ -0,0 +1,6 @@
+python ../metcalcpy/vertical_interp.py \
+    --datadir $DATA_DIR/TCRMW \
+    --input tc_rmw_dev_test_out.nc \
+    --config height_from_pressure_tcrmw.yaml \
+    --output tc_rmw_dev_test_vertical_interp.nc \
+    --debug
