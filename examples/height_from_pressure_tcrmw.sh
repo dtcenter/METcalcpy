2327ee291eee7a56187bbcc5d790360cc1255eb3
bikegeek <minnawin@ucar.edu>
2021-12-03 12:15:50 -0700

    Made changes to make user instructions easier

--- examples/height_from_pressure_tcrmw.sh
+++ examples/height_from_pressure_tcrmw.sh
@@ -1,6 +1,9 @@
+export DATA_DIR=/path/to/input-data
+export OUTPUT_DIR=/path/to/output
+
 python ../metcalcpy/vertical_interp.py \
-    --datadir $DATA_DIR/TCRMW \
-    --input tc_rmw_dev_test_out.nc \
+    --datadir $DATA_DIR \
+    --input tc_rmw_example.nc \
     --config height_from_pressure_tcrmw.yaml \
-    --output tc_rmw_dev_test_vertical_interp.nc \
+    --output $OUTPUT_DIR/tc_rmw_example_vertical_interp.nc \
     --debug
