6240d38f4c897ceda8446aa26c4a84c2dd0bb37b
bikegeek <3753118+bikegeek@users.noreply.github.com>
2022-02-25 14:07:42 -0700

    Feature 150 add copyright (#151)
    * #150 add UCAR/NCAR/RAP copyright to each Python file in util directory
    
    * #150 added UCAR/NCAR/RAL copyright to the Python code in the metcalcpy module
    
    * #150 added UCAR/NCAR/RAL and CIRES copyright info to these scripts that were provided by CIRES and modified by UCAR/NCAR/RAP/DTC
    
    * #150 added UCAR/NCAR/RAP University of Illinois Urbana-Champaign to code that was contributed by University of Illinois and modified by UCAR/NCAR/RAP/DTC
    
    * #150 add copyright text to code contributed by NOAA and modified by UCAR/NCAR/RAP/DTC
    
    * #150 add copyright for UCAR/NCAR/RAP for code contributed by NOAA and modified by UCAR/NCAR/RAP/DTC
    
    * #150 add copyright info to code contributed by NOAA and modified by UCAR/NCAR/RAL/DTC
    
    Co-authored-by: Minna Win <minnawin@seneca.rap.ucar.edu>

--- metcalcpy/contributed/blocking_weather_regime/Blocking.py
+++ metcalcpy/contributed/blocking_weather_regime/Blocking.py
@@ -1,3 +1,14 @@
+# ============================*
+ # ** Copyright UCAR (c) 2020
+ # ** University Corporation for Atmospheric Research (UCAR)
+ # ** National Center for Atmospheric Research (NCAR)
+ # ** Research Applications Lab (RAL)
+ # ** P.O.Box 3000, Boulder, Colorado, 80307-3000, USA
+ # ** University of Illinois, Urbana-Champaign
+ # ============================*
+ 
+ 
+ 
 import os
 import numpy as np
 import datetime
--- metcalcpy/contributed/spacetime/cross_spectra.py
+++ metcalcpy/contributed/spacetime/cross_spectra.py
@@ -1,3 +1,13 @@
+# ============================*
+ # ** Copyright UCAR (c) 2020
+ # ** University Corporation for Atmospheric Research (UCAR)
+ # ** National Center for Atmospheric Research (NCAR)
+ # ** Research Applications Lab (RAL)
+ # ** P.O.Box 3000, Boulder, Colorado, 80307-3000, USA
+ # ============================*
+ 
+ 
+ 
 """
 This is an example script for cross-spectral analysis. Computes power and cross-spectra for multiple input
 data sets.
