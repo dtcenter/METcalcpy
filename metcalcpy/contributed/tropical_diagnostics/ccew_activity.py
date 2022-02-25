81c82b6c3802055221c21989de959f5a8042caa6
Minna Win <minnawin@seneca.rap.ucar.edu>
2022-02-25 13:50:32 -0700

    #150 add copyright info to code contributed by NOAA and modified by UCAR/NCAR/RAL/DTC

--- metcalcpy/contributed/tropical_diagnostics/ccew_activity.py
+++ metcalcpy/contributed/tropical_diagnostics/ccew_activity.py
@@ -1,3 +1,13 @@
+# ============================*
+ # ** Copyright UCAR (c) 2020
+ # ** University Corporation for Atmospheric Research (UCAR)
+ # ** National Center for Atmospheric Research (NCAR)
+ # ** Research Applications Lab (RAL)
+ # ** P.O.Box 3000, Boulder, Colorado, 80307-3000, USA
+ # ============================*
+ 
+ 
+ 
 """
 Functions to project data onto CCEW EOFs and compute activity indices.
 Created by: Maria Gehne
