e2bb473a2b621b04fc5595bc32f6b763e0ec0e6c
Minna Win <minnawin@kiowa.rap.ucar.edu>
2022-03-01 12:04:36 -0700

    remove the --debug, not necessary unless explicitly requested

--- examples/height_from_pressure_tcrmw.sh
+++ examples/height_from_pressure_tcrmw.sh
@@ -1,6 +1,5 @@
 python ../metcalcpy/vertical_interp.py \
-    --datadir $DATA_DIR/TCRMW \
+    --datadir $DATA_DIR \
     --input tc_rmw_dev_test_out.nc \
     --config height_from_pressure_tcrmw.yaml \
-    --output tc_rmw_dev_test_vertical_interp.nc \
-    --debug
+    --output /home/minnawin/output/tc_rmw_dev_test_vertical_interp.nc \
