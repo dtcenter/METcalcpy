48b90cd8fa331c0457d8da0b339f79d9be8c3463
Minna Win <minnawin@kiowa.rap.ucar.edu>
2022-03-01 12:07:32 -0700

    inadvertently removed the --debug when it should be present, but commented out

--- examples/height_from_pressure_tcrmw.sh
+++ examples/height_from_pressure_tcrmw.sh
@@ -3,3 +3,4 @@ python ../metcalcpy/vertical_interp.py \
     --input tc_rmw_dev_test_out.nc \
     --config height_from_pressure_tcrmw.yaml \
     --output /home/minnawin/output/tc_rmw_dev_test_vertical_interp.nc \
+#   --debug
