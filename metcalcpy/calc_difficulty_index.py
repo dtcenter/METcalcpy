e967cc8ec952f6f631806ca7820a851aecb95356
bikegeek <3753118+bikegeek@users.noreply.github.com>
2022-03-18 09:38:25 -0600

    Update calc_difficulty_index.py
    added NRL to copyright

--- metcalcpy/calc_difficulty_index.py
+++ metcalcpy/calc_difficulty_index.py
@@ -3,6 +3,7 @@
  # ** University Corporation for Atmospheric Research (UCAR)
  # ** National Center for Atmospheric Research (NCAR)
  # ** Research Applications Lab (RAL)
+ # ** Naval Research Lab
  # ** P.O.Box 3000, Boulder, Colorado, 80307-3000, USA
  # ============================*
  
