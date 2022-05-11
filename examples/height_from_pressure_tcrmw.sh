78c6c9045fd95fdc55a1cc3421284e589a9be80e
bikegeek <3753118+bikegeek@users.noreply.github.com>
2022-05-11 11:06:12 -0600

    Update height_from_pressure_tcrmw.sh
    incorrect name of input file

--- examples/height_from_pressure_tcrmw.sh
+++ examples/height_from_pressure_tcrmw.sh
@@ -1,6 +1,6 @@
 python ../metcalcpy/vertical_interp.py \
     --datadir $DATA_DIR \
-    --input /path/to/input/tc_rmw_dev_test_out.nc \
+    --input /path/to/input/tc_rmw_example.nc \
     --config height_from_pressure_tcrmw.yaml \
     --output /path/to/output/tc_rmw_dev_test_vertical_interp.nc \
 #   --debug
