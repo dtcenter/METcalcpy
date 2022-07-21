afc715cef865864f7eed40b8c8dce31f6cc742f6
Tatiana Burek <tatiana@ucar.edu>
2022-07-21 12:22:35 -0600

    Feature 181 revision series (#190)
    * check if series_val_2 parameter exist. Need for the plots that don't support this feature  #179
    
    * check if the values that is used in  autocovariance calculation are not nan #181
    
    * check if series_val_2 parameter exist. Need for the plots that don't support this feature  #181
    
    * check if series_val_2 parameter exist. Need for the plots that don't support this feature  #181

--- metcalcpy/util/utils.py
+++ metcalcpy/util/utils.py
@@ -718,7 +718,7 @@ def perform_event_equalization(params, input_data):
 
     # if the second Y axis is present - run event equalizer on Y1
     # and then run event equalizer on Y1 and Y2 equalized data
-    if params['series_val_2']:
+    if 'series_val_2' in params.keys() and params['series_val_2']:
         # perform EE for each forecast variable on the axis 2
         output_ee_data_2 = \
             equalize_axis_data(fix_vals_keys, fix_vals_permuted_list, params, input_data, axis='2')
@@ -737,6 +737,7 @@ def perform_event_equalization(params, input_data):
                                         fix_vals_keys,
                                         fix_vals_permuted_list, True,
                                         params['line_type'] == "ssvar")
+
         output_ee_data = output_ee_data.drop('equalize', axis=1)
 
     return output_ee_data
