fixed the calculation of vcnt_anom_corr #200

metcalcpy/util/vcnt_statistics.py
@@ -516,16 +516,15 @@ def calculate_vcnt_dir_abser(input_data, columns_names, aggregation=False):
 def calculate_vcnt_anom_corr(input_data, columns_names, aggregation=False):
     warnings.filterwarnings('error')
     try:
-        total = get_total_values(input_data, columns_names, aggregation)
-        n = len(input_data)# n
-        fa_speed_bar = sum_column_data_by_name(input_data, columns_names, 'fa_speed_bar') / total  # f
-        oa_speed_bar = sum_column_data_by_name(input_data, columns_names, 'oa_speed_bar') / total  # o
-        uvffabar = sum_column_data_by_name(input_data, columns_names, 'uvffabar') / total  # ff
-        uvfoabar = sum_column_data_by_name(input_data, columns_names, 'uvfoabar') / total  # fo
-        uvooabar = sum_column_data_by_name(input_data, columns_names, 'uvooabar') / total  # oo
+        total = get_total_values(input_data, columns_names, aggregation) # n
+        fa_speed_bar = sum_column_data_by_name(input_data, columns_names, 'fa_speed_bar')   # f
+        oa_speed_bar = sum_column_data_by_name(input_data, columns_names, 'oa_speed_bar')   # o
+        uvffabar = sum_column_data_by_name(input_data, columns_names, 'uvffabar')   # ff
+        uvfoabar = sum_column_data_by_name(input_data, columns_names, 'uvfoabar')  # fo
+        uvooabar = sum_column_data_by_name(input_data, columns_names, 'uvooabar')   # oo
 
-        v = (n * uvffabar - fa_speed_bar * fa_speed_bar) * (n * uvooabar - oa_speed_bar * oa_speed_bar)
-        result = ((n * uvfoabar) - (fa_speed_bar * oa_speed_bar)) / np.sqrt(v)
+        v = (total * uvffabar - fa_speed_bar * fa_speed_bar) * (total * uvooabar - oa_speed_bar * oa_speed_bar)
+        result = ((total * uvfoabar) - (fa_speed_bar * oa_speed_bar)) / np.sqrt(v)
 
         # Check the computed range
         if result > 1:
@@ -542,10 +541,9 @@ def calculate_vcnt_anom_corr(input_data, columns_names, aggregation=False):
 def calculate_vcnt_anom_corr_uncntr(input_data, columns_names, aggregation=False):
     warnings.filterwarnings('error')
     try:
-        total = get_total_values(input_data, columns_names, aggregation)
-        uvffabar = sum_column_data_by_name(input_data, columns_names, 'uvffabar') / total  # ff
-        uvooabar = sum_column_data_by_name(input_data, columns_names, 'uvooabar') / total  # oo
-        uvfoabar = sum_column_data_by_name(input_data, columns_names, 'uvfoabar') / total  # fo
+        uvffabar = sum_column_data_by_name(input_data, columns_names, 'uvffabar')   # ff
+        uvooabar = sum_column_data_by_name(input_data, columns_names, 'uvooabar')   # oo
+        uvfoabar = sum_column_data_by_name(input_data, columns_names, 'uvfoabar')   # fo
 
         v = uvffabar * uvooabar
         result = uvfoabar / np.sqrt(v)