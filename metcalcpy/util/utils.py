f8f268f94febefe053ada80f861ca8d4c31b2988
TatianaBurek <tatiana@ucar.edu>
2022-11-04 12:28:33 -0600

    mae and mae_oerr as zero_perf_score_stats #229

--- metcalcpy/util/utils.py
+++ metcalcpy/util/utils.py
@@ -388,7 +388,7 @@ def perfect_score_adjustment(mean_stats_1, mean_stats_2, statistic, pval):
                              'VL1L2_RMSVE', 'PSTD_BRIER', 'PSTD_RELIABILITY',
                              'NBR_FBS', 'VL1L2_SPEED_ERR',
                              'NBR_POFD', 'NBR_FAR', 'NBR_ODDS', 'BCRMSE', 'ECNT_ME',
-                             'ECNT_RMSE', 'CRPS', 'ECNT_CRPS')
+                             'ECNT_RMSE', 'CRPS', 'ECNT_CRPS', 'ECNT_MAE', 'ECNT_MAE_OERR')
 
     one_perf_score_stats = ('ACC', 'FBIAS', 'PODY', 'PODN', 'CSI', 'GSS',
                             'HK', 'HSS', 'ORSS', 'EDS', 'SEDS',
