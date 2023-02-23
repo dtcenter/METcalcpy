92c6cf6f50801f1572f56aa98a56ad393174d107
bikegeek <3753118+bikegeek@users.noreply.github.com>
2023-02-22 17:32:22 -0700

    Bugfix 249 future warnings (#280)
    * Bump pillow from 8.3.2 to 9.3.0 (#260)
    
    Bumps [pillow](https://github.com/python-pillow/Pillow) from 8.3.2 to 9.3.0.
    - [Release notes](https://github.com/python-pillow/Pillow/releases)
    - [Changelog](https://github.com/python-pillow/Pillow/blob/main/CHANGES.rst)
    - [Commits](https://github.com/python-pillow/Pillow/compare/8.3.2...9.3.0)
    
    ---
    updated-dependencies:
    - dependency-name: pillow
      dependency-type: direct:production
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bugfix 251 test scorecard (#256)
    
    * Replace Python versions 3.8.6 and 3.8.12 with 3.8
    
    * Bugfix #251 replace SettingWithCopyWarning with Warning.  SettingWithCopyWarning no longer available for this check
    
    * Adding double asterisks to end bolding on mulitple lines
    
    Co-authored-by: Julie Prestopnik <jpresto@ucar.edu>
    
    * bugfix_249_warnings (#261)
    
    * Update unit_tests.yml
    
    replace 3.8.6 and 3.8.12 with 3.8 for Python because latest version of ubuntu does not have support for python 3.8.6
    
    * Feature 251 test scorecard (#253)
    
    * Issue #251 In Pandas 1.3 and above, the SettingWithCopy Warning category has been removed.  Replace the check for category=SettingWithCopyWarning with Warning, since SettingWithCopyWarning is a subclass of Warning
    
    * Added ending asterisks for end bold to fix these warnings: WARNING: Inline strong start-string without end-string.
    
    Co-authored-by: Julie Prestopnik <jpresto@ucar.edu>
    
    * Feature 250 test event equalize (#252)
    
    * Remove Python 3.8.6 and 3.8.12 and use Python 3.8 instead.  The latest version of ubuntu no longer has python 3.8.6 available.
    
    * Issue #250 fix the test_equalize_axis_data_no_fcst_var fixture
    
    * Added ending asterisks for end bold to fix these warnings: WARNING: Inline strong start-string without end-string.
    
    * #251 allow test to run check on numpy types regardless of numpy version
    
    * Update unit_tests.yml
    
    add python 3.10 to testing to evaluate package compatibility
    
    * Update unit_tests.yml
    
    remove Python 3.10 there are issues with HDF5
    
    Co-authored-by: Julie Prestopnik <jpresto@ucar.edu>
    
    * Delete requirements_36.txt
    
    Python 3.6 no longer supported
    
    * #249 Add test_future_warnings test
    
    * #249 additional sample data
    
    * Update requirements.txt
    
    https://github.com/dtcenter/METcalcpy/security/dependabot/22
    
    * Update requirements.txt
    
    https://github.com/dtcenter/METcalcpy/security/dependabot/20
    
    * #249 replacing pd.append with pd.concat and adding tests and data
    
    * #249 added tests
    
    * #249 Additional test data
    
    * Revert "Merge branch 'develop' of https://github.com/dtcenter/METcalcpy into bugfix_249_warnings"
    
    This reverts commit 7f203bface6be97f80512a2bc847ad38f28f0b2a, reversing
    changes made to dd87d77df51dd7144a5c32dc20b45e2f582c3fcc.
    
    Co-authored-by: Julie Prestopnik <jpresto@ucar.edu>
    
    * Adding file based on testing on Acorn for NCO (#265)
    
    * Per #267, updating modulefiles
    
    * Adding script for testing on WCOSS2
    
    * Correcting filename
    
    * Bugfix release (#272)
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Julie Prestopnik <jpresto@ucar.edu>
    Co-authored-by: Hank Fisher <fisherh@ucar.edu>

--- test/test_future_warnings.py
+++ test/test_future_warnings.py
@@ -8,7 +8,7 @@ import numpy as np
 from metcalcpy.util.utils import equalize_axis_data, aggregate_field_values, event_equalize
 from metcalcpy.agg_stat_event_equalize import AggStatEventEqualize
 from metcalcpy.util import pstd_statistics as pstd
-from metcalcpy.sum_stat import SumStat
+from metcalcpy.agg_stat_eqz import AggStatEventEqz
 from metcalcpy import GROUP_SEPARATOR, DATE_TIME_REGEX
 
 @pytest.fixture
@@ -204,6 +204,4 @@ def test_aggregate_field_values_agg_by_fcst_lead(settings_ee_dummy2):
         assert True
     except FutureWarning:
         assert False
-
-
 
