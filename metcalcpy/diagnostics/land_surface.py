8bbf0c5cb2dc3cf089c8f617214e6b2df270b623
Daniel Adriaansen <dadriaan@ucar.edu>
2024-04-04 16:32:53 +0000

    Removes cruft from testing, adds exception raising, and updates docstring formatting to match Sphinx https://sphinx-rtd-tutorial.readthedocs.io/en/latest/docstrings.html.

--- metcalcpy/diagnostics/land_surface.py
+++ metcalcpy/diagnostics/land_surface.py
@@ -1,37 +1,51 @@
 """Diagnostics relevant to Land/Surface applications"""
 
-def calc_tci(land_data,water_data):
-  """
+from xarray.core.dataarray import DataArray
+from pandas.core.series import Series
 
-  This function
+def calc_tci(soil_data,sfc_flux_data):
+  """ Function for computing the Terrestrial Coupling Index 
 
-  :param land data: The land data for the function, defaults to None
+  :param soil_data: The land data for the function
+  :type soil_data: Xarray DataArray, Pandas DataFrame
+  
+  :param sfc_flux_data: The surface flux data for the function
+  :type sfc_flux_data: Xarray DataArray, Pandas DataFrame
  
-  :type land_data: xarray dataarray(, optional)
+  :raises: TypeError
   
-  :raises ObjectError: Wrong Type
-  
-  :return: xarray
-  
-  :rtype: dataarray
-
+  :return: Terrestrial Coupling Index
+  :rtype: Xarray DataArray, Pandas DataFrame
   """
-  #  """
-  #  Function for computing the Terrestrial Coupling Index
-  #
-  #  Some more
-  #
-  #  """
 
-  return land_data-water_data
+  try:
+    # For Xarray objects, compute the mean 
+    if isinstance(soil_data,DataArray) and isinstance(sfc_flux_data,DataArray):
+      soil_mean = soil_data.mean(dim='time')
+      soil_count = soil_data.count(dim='time')
+      sfc_flux_mean = sfc_flux_data.mean(dim='time')
+      soil_std = soil_data.std(dim='time')
+      numer = ((soil_data-soil_mean) * (sfc_flux_data-sfc_flux_mean)).sum(dim='time')
 
+    # For Pandas objects, compute the mean
+    elif isinstance(soil_data,Series) and isinstance(sfc_flux_data,Series):
+      soil_mean = soil_data.mean()
+      soil_count = soil_data.count()
+      sfc_flux_mean = sfc_flux_data.mean()
+      soil_std = soil_data.std()
+      numer = ((soil_data-soil_mean) * (sfc_flux_data-sfc_flux_mean)).sum()
 
-def another_func(test):
-  """
-  Another function
+    # No other object types are supported
+    else:
+      raise TypeError
 
-  :param test: testing
+  except TypeError:
+    print("Only Xarray DataArray or Pandas DataFrame Objects are supported.\
+           Input objects must be of the same type.")
 
-  """
- 
-  return None 
+  # Compute the covariance term
+  covarTerm = numer / soil_count 
+
+  # Return the Terrestrial Coupling Index (TCI)
+  return covarTerm/soil_std
+
