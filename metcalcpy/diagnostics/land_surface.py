0d52ca4ab3803e0b8c388bfec17f669b2b3aaeab
Daniel Adriaansen <dadriaan@ucar.edu>
2024-04-04 15:47:13 +0000

    Adds a new land_surface diagnostic file to contain functions for land/surface applications.

---
+++ metcalcpy/diagnostics/land_surface.py
@@ -0,0 +1,37 @@
+"""Diagnostics relevant to Land/Surface applications"""
+
+def calc_tci(land_data,water_data):
+  """
+
+  This function
+
+  :param land data: The land data for the function, defaults to None
+ 
+  :type land_data: xarray dataarray(, optional)
+  
+  :raises ObjectError: Wrong Type
+  
+  :return: xarray
+  
+  :rtype: dataarray
+
+  """
+  #  """
+  #  Function for computing the Terrestrial Coupling Index
+  #
+  #  Some more
+  #
+  #  """
+
+  return land_data-water_data
+
+
+def another_func(test):
+  """
+  Another function
+
+  :param test: testing
+
+  """
+ 
+  return None 
