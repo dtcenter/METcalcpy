8a05512fb6a4490d69962046866f493cb4006c21
Daniel Adriaansen <dadriaan@ucar.edu>
2024-04-11 14:15:56 +0000

    Adds new skipna argument to the function, which is passed to pandas and xarray. The default is set to True, but a user can override if they wish.

--- metcalcpy/diagnostics/land_surface.py
+++ metcalcpy/diagnostics/land_surface.py
@@ -3,7 +3,7 @@
 from xarray.core.dataarray import DataArray
 from pandas.core.series import Series
 
-def calc_tci(soil_data,sfc_flux_data):
+def calc_tci(soil_data,sfc_flux_data,skipna=True):
   """ Function for computing the Terrestrial Coupling Index 
 
   :param soil_data: The land data for the function
@@ -11,31 +11,33 @@ def calc_tci(soil_data,sfc_flux_data):
   
   :param sfc_flux_data: The surface flux data for the function
   :type sfc_flux_data: Xarray DataArray, Pandas DataFrame
+
+  :param skipna: Skip NA values, passed to Pandas and Xarray
+  :type skipna: bool
  
   :raises: TypeError
   
   :return: Terrestrial Coupling Index
   :rtype: Xarray DataArray, Pandas DataFrame
 
-  Reference:
-  Dirmeyer, P. A., 2011: The terrestrial segment of soil moisture-climate coupling. *Geophys. Res. Lett.*, **38**, L16702, doi: 10.1029/2011GL048268.
+  :Reference: Dirmeyer, P. A., 2011: The terrestrial segment of soil moisture-climate coupling. *Geophys. Res. Lett.*, **38**, L16702, doi: 10.1029/2011GL048268.
   """
 
   # For Xarray objects, compute the mean 
   if isinstance(soil_data,DataArray) and isinstance(sfc_flux_data,DataArray):
-    soil_mean = soil_data.mean(dim='time')
+    soil_mean = soil_data.mean(dim='time',skipna=skipna)
     soil_count = soil_data.count(dim='time')
-    sfc_flux_mean = sfc_flux_data.mean(dim='time')
-    soil_std = soil_data.std(dim='time')
-    numer = ((soil_data-soil_mean) * (sfc_flux_data-sfc_flux_mean)).sum(dim='time')
+    sfc_flux_mean = sfc_flux_data.mean(dim='time',skipna=skipna)
+    soil_std = soil_data.std(dim='time',skipna=skipna)
+    numer = ((soil_data-soil_mean) * (sfc_flux_data-sfc_flux_mean)).sum(dim='time',skipna=skipna)
 
   # For Pandas objects, compute the mean
   elif isinstance(soil_data,Series) and isinstance(sfc_flux_data,Series):
-    soil_mean = soil_data.mean()
+    soil_mean = soil_data.mean(skipna=skipna)
     soil_count = soil_data.count()
-    sfc_flux_mean = sfc_flux_data.mean()
-    soil_std = soil_data.std()
-    numer = ((soil_data-soil_mean) * (sfc_flux_data-sfc_flux_mean)).sum()
+    sfc_flux_mean = sfc_flux_data.mean(skipna=skipna)
+    soil_std = soil_data.std(skipna=skipna)
+    numer = ((soil_data-soil_mean) * (sfc_flux_data-sfc_flux_mean)).sum(skipna=skipna)
 
   # No other object types are supported
   else:
