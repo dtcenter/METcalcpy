c7709a4f21e5c981074443afcee2c5e20e52083e
Dan Adriaansen <daniel.adriaansen@gmail.com>
2024-04-12 13:40:59 -0600

    Feature 364 add tci calc (#369)
    * Changes URL for coding standards to point to the METplus wrappers contributors guide coding standards.
    
    * Changes wording from build to make to be consistent with experience using newer sphinx.
    
    * Changes to allow autosummary to work and document the diagnostics submodule.
    
    * Adds new menu entry for the diagnostics submodule.
    
    * RST file for autosummary to use, with change from default module.rst file to get the functions to be clickable per sphinx-doc/sphinx#11998.
    
    * RST file for the diagnostic submodule.
    
    * Adds the generated directory from sphinx in the diag_api subdirectory to the gitignore.
    
    * Adds a new land_surface diagnostic file to contain functions for land/surface applications.
    
    * Adds __pycache__ to gitignore.
    
    * Adds sphinx execution times RST file to gitignore.
    
    * Removes cruft from testing, adds exception raising, and updates docstring formatting to match Sphinx https://sphinx-rtd-tutorial.readthedocs.io/en/latest/docstrings.html.
    
    * Restructure function to handle invalid types. This may not be the right way to do this.
    
    * Removes erroneous menu entries from testing documentation.
    
    * Adds test for calc_tci function and test data.
    
    * Adds Xarray and Pandas dependencies for building documentation, and lifts Python version for building documentation from 3.8 to 3.10.
    
    * Adds TCI test to the unit test config.
    
    * Changes name to remove API.
    
    * Trying to get GHA to run.
    
    * Updating pandas/xarray versions.
    
    * Double equals.
    
    * Changes RST heading level and adds description of the diagnostic reference.
    
    * Moves the diagnostic calculation reference to a sub-menu within the User's Guide.
    
    * Testing failure when readthedocs reports a warning.
    
    * Adds back xarray readthedocs requirement.
    
    * Update docs/Contributors_Guide/index.rst
    
    Co-authored-by: Julie Prestopnik <jpresto@ucar.edu>
    
    * Adds reference for  function.
    
    * Adds bool for turning on/off the pandas and xarray tests.
    
    * Adds new skipna argument to the function, which is passed to pandas and xarray. The default is set to True, but a user can override if they wish.
    
    * Renames diag_api directory to diag_ref.
    
    * Renames testing file for land_surface diagnostics to support additional land_surface diagnostics in the future.
    
    * Turns on the Sphinx napoleon extension to support Google and NumPy docstrings and re-writes the  docstring using Google format.
    
    * Updates Google docstring to acheive desired formatting.
    
    * Adds dunder init file.
    
    * Renames hyperlink and menu entry.
    
    * Changed a directory name and forgot to update the ignore file.
    
    * Removes directory not intended to be in repository.
    
    ---------
    
    Co-authored-by: Julie Prestopnik <jpresto@ucar.edu>

---
+++ docs/diag_ref/index.rst
@@ -0,0 +1,18 @@
+.. _api-index:
+
+********************************
+Diagnostic Calculation Reference
+********************************
+
+This reference contains technical documentation for various diagnostic calculations in METcalcpy.
+
+.. currentmodule:: metcalcpy.diagnostics
+
+.. autosummary::
+   :toctree: generated/
+   :recursive:
+
+   land_surface
+  
+* :ref:`modindex`
+* :ref:`genindex`
---
+++ test/test_diagnostics_land_surface.py
@@ -0,0 +1,56 @@
+"""Tests the functions in diagnostics/land_surface.py"""
+
+from metcalcpy.diagnostics.land_surface import calc_tci
+from xarray.testing import assert_equal
+import xarray as xr
+import pandas as pd
+import numpy as np
+
+__author__ = "Daniel Adriaansen (NCAR)"
+
+def test_calc_tci():
+  """
+  Test that the output of the calc_tci function is correct.
+
+  Returns
+  -------
+  None.
+  """
+
+  doXarray = True
+  doPandas = True
+
+  if doXarray:
+    ###### Xarray DataArray case
+    # Input data for Xarray case
+    xr_input = xr.open_dataset('data/calc_tci_jja_xarray_input.nc')
+
+    # Output data for Xarray case
+    xr_truth_var = '__xarray_dataarray_variable__'
+    xr_truth = xr.open_dataset('data/calc_tci_jja_xarray_output.nc')
+ 
+    # Compute TCI
+    xr_test = calc_tci(xr_input['SOILWATER_10CM'],xr_input['LHFLX'])
+
+    # Validate Xarray case
+    assert_equal(xr_truth[xr_truth_var],xr_test)
+
+  if doPandas:
+    ###### Pandas DataFrame case
+    # Input data for Pandas case
+    pd_input = pd.read_csv('data/calc_tci_jja_pandas_input.csv')
+ 
+    # There are three sites in the test data, each should have its own TCI value
+    pd_test = np.array([])
+    for name,site in pd_input.groupby('station_id'):
+      pd_test = np.append(pd_test,calc_tci(site['SWC_F_MDS_1'],site['LE_F_MDS']))
+
+    # The truth values
+    pd_truth = np.array([-1.851168960504201,11.861239905560712,-2.0781980819945076])
+
+    # Validate Pandas case
+    for test,truth in tuple(zip(pd_test,pd_truth)):
+      assert test==truth
+    
+if __name__ == "__main__":
+  test_calc_tci()  
