d68749d44cb045f643304ed7bbc1d584720ff56f
Julie Prestopnik <jpresto@ucar.edu>
2024-10-18 08:04:27 -0600

    Updating release date

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ version = 'release'
 verinfo = version
 release = f'{version}'
 release_year = '2024'
-release_date = f'{release_year}-10-17'
+release_date = f'{release_year}-10-18'
 copyright = f'{release_year}, {author}'
 
 # if set, adds "Last updated on " followed by
