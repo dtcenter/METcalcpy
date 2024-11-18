ce085596acf36fc06d0b3278453b32f9548db989
John Sharples <john.sharples@bom.gov.au>
2024-11-18 15:09:07 +1100

    401: Add basic tests for untested modules

---
+++ test/test_agg_stat_bootstrap.py
@@ -0,0 +1,15 @@
+import metcalcpy.agg_stat_bootstrap as asb
+from test.utils import AGG_STAT_AND_BOOT_DATA
+
+
+DEFAULT_CONF = {
+    "log_dir": "/tmp",
+    "log_filename": "tmp.log",
+    "log_level": "DEBUG",
+    "agg_stat_input": AGG_STAT_AND_BOOT_DATA,
+}
+
+
+def test_smoke():
+    """Basic test to check object instantiation"""
+    my_asb = asb.AggStatBootstrap(DEFAULT_CONF)
