a43b7f8951c0bbb6a87d3944225eb3634f77205d
John Sharples <john.sharples@bom.gov.au>
2024-11-18 16:03:28 +1100

    401: more sonarqube fixes

--- test/test_agg_stat_bootstrap.py
+++ test/test_agg_stat_bootstrap.py
@@ -3,13 +3,13 @@ from test.utils import AGG_STAT_AND_BOOT_DATA
 
 
 DEFAULT_CONF = {
-    "log_dir": "/tmp",
     "log_filename": "tmp.log",
     "log_level": "DEBUG",
     "agg_stat_input": AGG_STAT_AND_BOOT_DATA,
 }
 
 
-def test_smoke():
+def test_smoke(tmp_path):
     """Basic test to check object instantiation"""
+    DEFAULT_CONF["log_dir"] = tmp_path
     asb.AggStatBootstrap(DEFAULT_CONF)
