382bc7f72b803c2740c798a11bd41fd57d34094e
bikegeek <minnawin@ucar.edu>
2025-07-09 17:35:20 -0600

    Fixes that were made in 3.1(6.1 METplus) but not in 3.0 (6.0 METplus)

--- metcalcpy/agg_stat_bootstrap.py
+++ metcalcpy/agg_stat_bootstrap.py
@@ -302,12 +302,13 @@ class AggStatBootstrap:
             # build a data frame with the sampled data
             data_cases = np.asarray(self.series_data['case'])
             flat_cases = cases.flatten()
-            values = self.series_data[np.in1d(data_cases, flat_cases)].to_numpy()
+            values = self.series_data[np.isin(data_cases, flat_cases)].to_numpy()
             safe_log(logger, "debug", f"Number of values selected for single case: {len(values)}")
             # Calculate the statistic for each bootstrap iteration
+            stat_values = []
             try:
                 stat_value = globals()[func_name](values, self.column_names, logger=logger)
-                stat_values.append([stat_value])
+                stat_values.append(stat_value)
                 safe_log(logger, "info", f"Statistic calculated for bootstrap iteration: {stat_value}")
             except Exception as e:
                 safe_log(logger, "error", f"Error calculating statistic for bootstrap iteration: {e}")
@@ -323,7 +324,7 @@ class AggStatBootstrap:
                 # Calculate the statistic for each bootstrap iteration
                 try:
                     stat_value = globals()[func_name](values, self.column_names, logger=logger)
-                    stat_values.append([stat_value])
+                    stat_values.append(stat_value)
                     safe_log(logger, "info", f"Statistic calculated for bootstrap iteration: {stat_value}")
                 except Exception as e:
                     safe_log(logger, "error", f"Error calculating statistic for bootstrap iteration: {e}")
