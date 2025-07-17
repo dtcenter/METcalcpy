6caca264a5f94f131c7e1870dc705538dc2e1b0e
MWin <3753118+bikegeek@users.noreply.github.com>
2025-07-16 19:06:20 -0600

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ version = '3.1.0'
 verinfo = version
 release = f'{version}'
 release_year = '2025'
-release_date = f'{release_year}-06-30'
+release_date = f'{release_year}-07-16'
 copyright = f'{release_year}, {author}'
 
 # if set, adds "Last updated on " followed by
