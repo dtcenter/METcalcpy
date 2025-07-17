a1f9cc46690fc154d9547032b2e42ad7a84d0ea4
MWin <3753118+bikegeek@users.noreply.github.com>
2025-07-16 18:13:24 -0700

    Updates for coordinated release (#447)
    * Update conf.py
    
    updated for 3.2.0-beta1 development cycle
    
    * Update version
    
    update version for 3.2.0 beta1 development cycle
    
    * Feature 436 installation files (#441)
    
    * Per #436, update Orion file for Python 3.12
    
    * Per #436, updating Casper file for Python 3.12
    
    * Per #436, updating compiler for orion
    
    * Per #436, update hera file for Python 3.12
    
    * Per #436, update jet file for Python 3.12
    
    * Making index consistent with other METplus repositories and initial commit of overview (#442)
    
    * Address xarray FutureWarning for open_dataset by setting decode_timedelta=True to maintain the current default behavior.  Create the output directory if it doesn't already exist to avoid error messages when attempting to write the output dataset. (#445)
    
    * Update height_from_pressure_tcrmw.sh
    
    remove the /path/to portion of the input arg
    
    * Updates for coordinated release
    
    * Replace deprecated numpy 'is1d' with 'isin'
    
    * Test numpy deprecation (#450)
    
    * Making index consistent with other METplus repositories and initial commit of overview (#443)
    
    * Replace deprecated numpy 'in1d' with 'isin'
    
    ---------
    
    Co-authored-by: Julie Prestopnik <jpresto@ucar.edu>
    
    * Adding new modulefile for ursa
    
    * Update docs/Users_Guide/release-notes.rst
    
    Co-authored-by: Julie Prestopnik <jpresto@ucar.edu>
    
    * Update docs/Users_Guide/release-notes.rst
    
    Co-authored-by: Julie Prestopnik <jpresto@ucar.edu>
    
    * Update conf.py
    
    ---------
    
    Co-authored-by: Julie Prestopnik <jpresto@ucar.edu>

--- docs/conf.py
+++ docs/conf.py
@@ -22,11 +22,11 @@ project = 'METcalcpy'
 copyright = '2025, NSF NCAR'
 author = 'UCAR/NSF NCAR, NOAA, CSU/CIRA, and CU/CIRES'
 author_list = 'Fisher, H., C. Kalb, D. Adriaansen, D. Fillmore, M. Win-Gildenmeister, T. Burek, M. Smith, and T. Jensen'
-version = '3.1.0-rc1'
+version = '3.2.0-rc1'
 verinfo = version
 release = f'{version}'
 release_year = '2025'
-release_date = f'{release_year}-05-22'
+release_date = f'{release_year}-07-16'
 copyright = f'{release_year}, {author}'
 
 # if set, adds "Last updated on " followed by
