bcc8769d3588b7c9ce4e2e4fa3b5b3277ebaa78e
bikegeek <minnawin@ucar.edu>
2025-07-30 10:37:26 -0600

    Remove hard-coded paths

--- examples/height_from_pressure_tcrmw.sh
+++ examples/height_from_pressure_tcrmw.sh
@@ -1,6 +1,8 @@
+export DATA_DIR=/path/to/data
+export INPUT=tc_rmw_example.nc
+export OUTPUT=/path/to/output/directory
 python ../metcalcpy/vertical_interp.py \
     --datadir $DATA_DIR \
-    --input /path/to/input/tc_rmw_example.nc \
+    --input $INPUT \
     --config height_from_pressure_tcrmw.yaml \
-    --output /path/to/output/tc_rmw_dev_test_vertical_interp.nc \
-#   --debug
+    --output $OUTPUT/tc_rmw_dev_test_vertical_interp.nc
