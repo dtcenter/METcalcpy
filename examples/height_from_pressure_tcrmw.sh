eb2411c49eb2785a584d6f250468c566d7398a4c
MWin <3753118+bikegeek@users.noreply.github.com>
2025-07-30 12:24:53 -0600

    Feature 444 vert interp (#452)
    * Address xarray FutureWarning for open_dataset by setting decode_timedelta=True to maintain the current default behavior.  Create the output directory if it doesn't already exist to avoid error messages when attempting to write the output dataset.
    
    * Added relative humidity to config file to enable plotting RH from example data
    
    * Remove hard-coded paths

--- examples/height_from_pressure_tcrmw.sh
+++ examples/height_from_pressure_tcrmw.sh
@@ -1,6 +1,8 @@
+export DATA_DIR=/path/to/data
+export INPUT=tc_rmw_example.nc
+export OUTPUT=/path/to/output/directory
 python ../metcalcpy/vertical_interp.py \
     --datadir $DATA_DIR \
     --input tc_rmw_example.nc \
     --config height_from_pressure_tcrmw.yaml \
-    --output /path/to/output/tc_rmw_dev_test_vertical_interp.nc \
-#   --debug
+    --output $OUTPUT/tc_rmw_dev_test_vertical_interp.nc
