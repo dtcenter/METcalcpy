eae0883c2bf7501f3c8b28e8f89aaeb2205af012
Julie Prestopnik <jpresto@ucar.edu>
2025-11-14 09:29:22 -0700

    Per #459, update version, authors, and release notes. (#460)

--- docs/conf.py
+++ docs/conf.py
@@ -21,12 +21,12 @@ print(sys.path)
 project = 'METcalcpy'
 copyright = '2025, NSF NCAR'
 author = 'UCAR/NSF NCAR, NOAA, CSU/CIRA, and CU/CIRES'
-author_list = 'Fisher, H., C. Kalb, D. Adriaansen, D. Fillmore, M. Win-Gildenmeister, T. Burek, M. Smith, and T. Jensen'
-version = '3.2.0-rc1'
+author_list = 'Kalb, C., D. Adriaansen, D. Fillmore, M. Win-Gildenmeister, T. Burek, M. Smith, T. Jensen, and H. Fisher'
+version = '3.2.0'
 verinfo = version
 release = f'{version}'
 release_year = '2025'
-release_date = f'{release_year}-09-29'
+release_date = f'{release_year}-11-14'
 copyright = f'{release_year}, {author}'
 
 # if set, adds "Last updated on " followed by
