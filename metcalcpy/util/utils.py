ab8a0fa4727d1f444d7f4676ff8076325350c9a1
bikegeek <minnawin@ucar.edu>
2026-08-16 16:51:22 -0600

    make values in val1 and val2 floats, they can sometimes get passed in as str in the dataframe

--- metcalcpy/util/utils.py
+++ metcalcpy/util/utils.py
@@ -162,6 +162,10 @@ def calc_derived_curve_value(val1, val2, operation, logger=None):
         safe_log(logger, "warning", "Input values are invalid: None detected or lengths mismatch.")
         return None
 
+    # make sure the values in val1 and val2 are floats
+    val1 = [float(v) for v in val1]]
+    val2 = [float(v) for v in val2]]
+
     result_val = None
     if operation in ('DIFF', 'DIFF_SIG'):
         result_val = [a - b for a, b in zip(val1, val2)]
